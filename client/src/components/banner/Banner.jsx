17346bb28b865f12eefb556e76ab198874727e4c
Szijártó Ferenc <46969034+szijartoferenc@users.noreply.github.com>
2025-05-10 18:07:19 +0200

    Banner.jsx

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography, styled } from '@mui/material';
 
 const Image = styled(Box)`
-    background: url(https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg) center/55% repeat-x #000;
+    background: url(https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg) center/55% repeat-x #000;
     width: 100%;
     height: 50vh;
     display: flex;
